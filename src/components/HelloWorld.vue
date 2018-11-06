154f3b80fc5c12a43ace522ab7b263ed648a810a
zhoujun7 <1445722992@qq.com>
2018-11-06 22:21:40 +0800

    123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <ul></ul>
+  <div>
+    <ul>
+      <li v-text="msg">{{msg}}</li>
+      <li v-for="item in tiems" :key="item.id">{{item.name}}<li/>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data:{
+    msg:"this is my todolist",
+    tiems:[{id:1,name:"coding"},{id:2,name:"paobu"},{id:3,name:"paobu"}]
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
