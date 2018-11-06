de42bafe9c8e6598b9cad3eaf3a541c3ea36c1ec
zhoujun7 <1445722992@qq.com>
2018-11-06 23:03:15 +0800

    mytodolist

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,38 @@
   <div>
     <ul>
       <li v-text="msg">{{msg}}</li>
-      <li v-for="item in tiems" :key="item.id">{{item.name}}<li/>
+      <input v-model="newitem"  v-on:keyup.enter="additem">
+      <li v-for="item in tiems" :key="item.id" v-bind:class="{finished:item.istrue}" v-on:click="ontuer(item)">{{item.name}}<li/>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  data:{
-    msg:"this is my todolist",
-    tiems:[{id:1,name:"coding"},{id:2,name:"paobu"},{id:3,name:"paobu"}]
+  data(){
+    return{
+      msg:"this is my todolist",
+      tiems:[],
+      newitem:''
+    }
+  },
+  methods:{ 
+    additem:function()
+    {
+      this.tiems.push({name:this.newitem,istrue:false});
+      this.newitem='';
+    },
+    ontuer:function(item)
+    {
+        item.istrue=!item.istrue;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.finished{
+  text-decoration: underline;
+}
 </style>
